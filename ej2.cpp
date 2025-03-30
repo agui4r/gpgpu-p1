7a7f09e69c581f4faf7e03be99753f90e75c23e4
Camilo Montes de Oca <camilomdeoca@gmail.com>
2025-03-30 20:00:35 -0300

    add progress and pull main into another function

--- ej2.cpp
+++ ej2.cpp
@@ -10,10 +10,14 @@ void matrix_mult (vector<vector<float>> A, vector<vector<float>> B, vector<vecto
     for (size_t i = 0; i < size; i++)
         for (size_t j = 0; j < size; j++) C[i][j] = 0;
 
-    for (size_t i = 0; i < size; i++)
+    for (size_t i = 0; i < size; i++) 
+    {
+        printf("%zu/%zu             \r", i, size);
         for (size_t j = 0; j < size; j++)
             for (size_t k = 0; k < size; k++)
                 C[i][j] += A[i][k] * B[k][j];
+    }
+    printf("\n");
 }
 
 void matrix_mult_cache_optimized (vector<vector<float>> A, vector<vector<float>> B, vector<vector<float>>& C, size_t size) {
@@ -21,22 +25,31 @@ void matrix_mult_cache_optimized (vector<vector<float>> A, vector<vector<float>>
         for (size_t j = 0; j < size; j++) C[i][j] = 0;
 
     for (size_t j = 0; j < size; j++)
+    {
+        printf("%zu/%zu             \r", j, size);
         for (size_t jj = 0; jj < size; jj += TAM_BL)
             for (size_t i = 0; i < size; i++)
                 for(size_t k = jj; k < size && k < jj + TAM_BL; k++)
                     C[i][j] += A[i][k] * B[k][j];
+    }
+    printf("\n");
 }
 
-int main(int argc, char* argv[]) {
-    constexpr size_t size = 2500;
+int run_with_size(size_t size) {
     vector<vector<float>> A(size, vector<float>(size))
         , B(size, vector<float>(size))
         , C(size, vector<float>(size));
+
+    printf("SIZE = %zu\n", size);
 
     //init arrays
     for (size_t i = 0; i < size; i++) {
         for (size_t j = 0; j < size; j++) {
             A[i][j] = i + j;
+        }
+    }
+    for (size_t i = 0; i < size; i++) {
+        for (size_t j = 0; j < size; j++) {
             B[i][j] = i * j + i + j;
         }
     }
@@ -60,12 +73,29 @@ int main(int argc, char* argv[]) {
     // }
 
     chrono::time_point<chrono::high_resolution_clock> start, end;
+    unsigned long elapsed;
+
+    //start = chrono::high_resolution_clock::now();
+    //matrix_mult_cache_optimized(A, B, C, size);
+    //end = chrono::high_resolution_clock::now();
+
+    //elapsed = chrono::duration_cast<chrono::microseconds>(end - start).count();
+    //printf ("Cache optimized matrix multiplication time = %f miliseconds\n", static_cast<double>(elapsed) / 1000.0);
+    //printf ("Cache optimized matrix multiplication MFLOPS = %f\n", (size*size*size) / static_cast<double>(elapsed));
+
+    // cout << "Cache optimized matrix multiplication result: " << endl;
+    // for (size_t i = 0; i < size; i++) {
+    //     for (size_t j = 0; j < size; j++) {
+    //         cout << C[i][j] << "  ";
+    //     }
+    //     cout << endl;
+    // }
 
     start = chrono::high_resolution_clock::now();
     matrix_mult(A, B, C, size);
     end = chrono::high_resolution_clock::now();
 
-    unsigned long elapsed = chrono::duration_cast<chrono::microseconds>(end - start).count();
+    elapsed = chrono::duration_cast<chrono::microseconds>(end - start).count();
     printf ("Normal matrix multiplication time = %f miliseconds\n", static_cast<double>(elapsed) / 1000.0);
     printf ("Normal matrix multiplication MFLOPS = %f\n", (size*size*size) / static_cast<double>(elapsed));
 
@@ -77,21 +107,12 @@ int main(int argc, char* argv[]) {
     //     cout << endl;
     // }
 
-    start = chrono::high_resolution_clock::now();
-    matrix_mult_cache_optimized(A, B, C, size);
-    end = chrono::high_resolution_clock::now();
+    return 0;
+}
 
-    elapsed = chrono::duration_cast<chrono::microseconds>(end - start).count();
-    printf ("Cache optimized matrix multiplication time = %f miliseconds\n", static_cast<double>(elapsed) / 1000.0);
-    printf ("Cache optimized matrix multiplication MFLOPS = %f\n", (size*size*size) / static_cast<double>(elapsed));
-
-    // cout << "Cache optimized matrix multiplication result: " << endl;
-    // for (size_t i = 0; i < size; i++) {
-    //     for (size_t j = 0; j < size; j++) {
-    //         cout << C[i][j] << "  ";
-    //     }
-    //     cout << endl;
-    // }
-
+int main (int argc, char *argv[]) {
+    run_with_size(100);
+    run_with_size(200);
+    run_with_size(1000);
     return 0;
 }
